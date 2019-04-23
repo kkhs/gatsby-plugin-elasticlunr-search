fb93f7241c831c43c46bdddaab43b24f5de22034
Satoshi Ebihara <s.ebihara@kakehashi.life>
2019-04-23 19:45:49 +0900

    WIP

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -97,7 +97,7 @@ exports.sourceNodes = async ({ getNodes, actions }) => {
   const { touchNode } = actions
 
   const existingNodes = getNodes().filter(
-    n => n.internal.owner === `@gatsby-contrib/gatsby-plugin-elasticlunr-search`
+    n => n.internal.owner === `@kkhs/gatsby-plugin-elasticlunr-search`
   )
   existingNodes.forEach(n => touchNode({ nodeId: n.id }))
 }
